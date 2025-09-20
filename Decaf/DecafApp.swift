9b84612a1cd3936a89db54426484cb3cf90b9a69
nullptr <3621629+0x0L@users.noreply.github.com>
2025-09-20 19:46:09 +0200

    Initial Commit

---
+++ Decaf/DecafApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecafApp.swift
+//  Decaf
+//
+//  Created by nullptr on 20/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct DecafApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
